4ed73597e60b92afe64847c39159ef1c8acc1645
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-05-19 12:52:27 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,110 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onImgClick);
 
-console.log(galleryItems);
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+// create modal
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const isItemImage = e.target.classList.contains("gallery__image");
+  if (!isItemImage) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+    instance.close();
+  }
+}
+
+// console.log(galleryItems);
+// const galleryItemsUl = document.querySelector(".gallery");
+// const createItems = galleryItems.map(({ preview, original, description }) => {
+//   const galleryList = `<li class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`;
+//   return galleryList;
+// });
+
+// galleryItemsUl.insertAdjacentHTML(`beforeend`, createItems.join(``));
+
+// galleryItemsUl.addEventListener(`click`, handleImgClick);
+// function handleImgClick(event) {
+//   event.preventDefault();
+//   const imgTarget = event.target;
+//   if (imgTarget.nodeName !== `IMG`) {
+//     return imgTarget;
+//   }
+//   const largeImage = imgTarget.dataset.source;
+//   openModalWindow(largeImage);
+// }
+// let instance = null;
+// function openModalWindow(imageUrl) {
+//   const imageElement = `<img src="${imageUrl}">`;
+
+//   instance = basicLightbox.create(imageElement, {
+//     onClose: closeModalWindow,
+//   });
+//   instance.show();
+
+//   document.addEventListener("keydown", handleKeyDown);
+// }
+
+// function closeModalWindow() {
+//   if (instance) {
+//     instance.close();
+//   }
+//   document.removeEventListener("keydown", handleKeyDown);
+// }
+
+// function handleKeyDown(event) {
+//   if (event.code === "Escape") {
+//     closeModalWindow();
+//   }
+// }
